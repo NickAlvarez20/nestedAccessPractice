f3759f5878ede6aee584c5553aee7a600fa24e52
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2025-12-22 19:56:15 -0700

    add and refactor: Add function to sum Grades using for of... and Object.entries with [key, value] access, refactor and store Object.entries() in entries var

---
+++ sumObjects.js
@@ -0,0 +1,29 @@
+const Astudent = {
+  math: 95,
+  programming: 95,
+};
+
+const sumGrades = (student) => {
+  let sum = 0;
+  for (const [key, value] of Object.entries(student)) {
+    sum += value;
+  }
+  return { "Average Grade": sum / Object.entries(student).length };
+};
+
+// Refactor
+const sumGradesNotExpensive = (student) => {
+  const entries = Object.entries(student);
+  let sum = 0;
+  for (const [key, value] of entries) {
+    sum += value;
+  }
+  if (entries.length === 0) {
+    return { "Average Grade": 0 };
+  }
+  return { "Average Grade": sum / entries.length };
+};
+
+console.log(sumGrades(Astudent));
+
+console.log(sumGradesNotExpensive(Astudent));
